c0b391aa2f6a6bad32991b180c6085d652c4e036
regisemiel <regisemiel@yandex.ru>
2025-01-14 20:36:32 +0300

    Initial commit

---
+++ src/main/java/com/example/docker_java_simple/DockerJavaSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_java_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJavaSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker_java_simple/DockerJavaSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_java_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
